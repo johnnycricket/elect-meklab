d685777222ca555774efd23cb3ac4ab3b4beebba
Vorwald John R <VorwaldJohnR@JohnDeere.com>
2024-02-26 20:29:08 -0600

    finally got electron forge and vite and typescript to be happy. can open a blank app.

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,8 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.ts'),
     },
   });
 
--- vite.main.config.js
+++ vite.main.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite';
 // eslint-disable-next-line import/no-unresolved
-import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config
 export default defineConfig({
+  base: process.env.DEV_ENV == 'true' ? '/' : './',
   resolve: {
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     mainFields: ['module', 'jsnext:main', 'jsnext'],
   },
-  plugins: [
-    svelte({
-      configFile: 'svelte.config.js'
-    })
-  ]
+  build: {
+    rollupOptions: {
+      external: [
+        'svelte'
+      ]
+    },
+    outDir: '.vite/build'
+  }
 });
