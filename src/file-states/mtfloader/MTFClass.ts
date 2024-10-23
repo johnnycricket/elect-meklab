182b3c5587a3d59353e1d9ff3284625b643fa7a9
Vorwald John R <VorwaldJohnR@JohnDeere.com>
2024-10-23 14:02:06 -0500

    more tests, more refining the parsing of a mech mtf file

--- src/file-states/mtfloader/MTFClass.ts
+++ src/file-states/mtfloader/MTFClass.ts
@@ -15,7 +15,7 @@ interface RetObj {
     engine: string,
     structure: string,
     myomer: string,
-    heatsinks: string,
+    'heat sinks': string,
     walk: number,
     run: number | undefined,
     jump: number | undefined,
@@ -35,8 +35,10 @@ interface RetObj {
 }
 export class MTFClass {
     private colonRegExp = new RegExp(/:/gi);
-    private shallowRegExp = new RegExp(/(chassis)|(model)|(mul)|(config)|(techbase)|(era)|(source)|(rules)|(role)|(quirk)|(mass)|(engine)|(structure)|(myomer)|(heat sinks)|(walk)|(run)|(jump)/gi);
+    private shallowRegExp = new RegExp(/(chassis)|(model)|(mul)|(config)|(techbase)|(era)|(source)|(rules)|(role)|(quirk)|(mass)|^(engine)|(structure)|(myomer)|(heat sinks)|(walk)|(run)|(jump)$/gi);
     private multilineOptions = ['Armor', 'Weapons', 'Left Arm', 'Right Arm', 'Left Torso', 'Right Torso', 'Center Torso', 'Head', 'Left Leg', 'Right Leg'];
+    private deepRegExp = new RegExp(/(armor)|(weapons)|(left arm)|(right arm)|(left torso)|(right torso)|(center torso)|(head)|(left leg)|(right leg)/gi);
+
 
     private singeLines(line: string): string[] {
         const isColon = String(line.match(this.colonRegExp));
@@ -54,48 +56,46 @@ export class MTFClass {
         return keyvalTuple;
     }
 
-    private multiLines(items: string[][]): string[][] {
-        const multis = this.multilineOptions.filter((opt, i) => {
-            let dist = 0;
-            const multi = ['', []];
+    private multiLines(items: string[][]): (string | string[][])[][] {
+        const collectedMultis: (string | string[][])[][] = [];
+        this.multilineOptions.filter((opt, i) => {
             const start = items.findIndex((item) => {
                 return item[0] === opt;
             });
-            const end = items.findIndex((item) => {
-                return item[0] === this.multilineOptions[i+1];
-            });
-            dist = start;
+            let end: number;
 
-            multi[0] = items;
-            while( dist < end) {
+            if(opt === 'Weapons'){
+                const skipAhead = parseInt(items[start + 1][1]) * 2;
+                end = start + skipAhead;
+            } else {
+                end = items.findIndex((item) => {
+                    return item[0] === this.multilineOptions[i+1];
+                });
+            }
+            
+            const collected = [];
+            const count = start;
+
+            while(count < end) {
+                if(opt === 'Weapons') {
+                    const weap = items[count][0];
+                    const next = count + 1;
+                    const loc = items[next][0]
+                    collected.push([weap, loc]);
+                    count + 2;
+                } else {
+                    collected.push(items[count]);
+                }
                 
             }
-
+            collectedMultis.push([opt, collected]);
         });
         
-        return;
+        return collectedMultis;
     }
 
-    private shallowPop(dataArray: string[][]): RetObj {
-        const shallow: RetObj = {
-            chassis: '',
-            model: '',
-            mul: 0,
-            config: '',
-            techbase: '',
-            era: '',
-            source: '',
-            rules: '',
-            role: '',
-            quirk: [],
-            mass: 0,
-            engine: '',
-            structure: '',
-            myomer: '',
-            heatsinks: '',
-            walk: 0,
-            run: 0,
-            jump: 0,
+    private deepPop(dataArray: (string | string[][])[][]): Partial<RetObj> {
+        const deep: Partial<RetObj> = {
             armortype: "",
             armor: [],
             arms: [],
@@ -110,22 +110,72 @@ export class MTFClass {
                 rl: []
             }
         };
+        const armsArray: string[] = [];
+        
+        dataArray.filter((item) => {
+            console.log(item)
+            
+            const zerozero = item[0] as string;
+            const found = zerozero.match(this.deepRegExp) as string[];
+            const foundValues = item[1];
+            const foundKey = found[0].toLowerCase() as unknown as keyof RetObj;
+
+            if(found?.includes('Armor')) {
+                deep['armortype'] = foundValues[0][1];
+                deep[foundKey] = foundValues as never;
+            } else if(found?.includes('Weapons')) {
+                deep['arms'] = foundValues as never;
+            } else {
+                deep[foundKey] = foundValues as never;
+            }
+        })
+        
+        return deep
+    }
+
+    private shallowPop(dataArray: string[][]): Partial<RetObj> {
+        const shallow: Partial<RetObj> = {
+            chassis: '',
+            model: '',
+            mul: 0,
+            config: '',
+            techbase: '',
+            era: '',
+            source: '',
+            rules: '',
+            role: '',
+            quirk: [],
+            mass: 0,
+            engine: '',
+            structure: '',
+            myomer: '',
+            'heat sinks': '',
+            walk: 0,
+            run: 0,
+            jump: 0,
+        };
+        const draftQuirks: string[] = [];
 
         dataArray.filter((item) => {
             const found = item[0].match(this.shallowRegExp);
             const foundValue = item[1];
-            const foundKey = found as unknown as keyof RetObj;
+            const foundKey = found?.toString().toLowerCase() as unknown as keyof RetObj;
+            
             if(found?.includes('quirk')) {
-                shallow['quirk'].push(foundValue)
+                draftQuirks.push(foundValue)
             } else {
                 shallow[foundKey] = foundValue as never;
             }
         });
 
+        if(draftQuirks.length > 0) {
+            shallow.quirk = draftQuirks;
+        }
+
         return shallow;
     }
 
-    public reader(contents: string): RetObj {
+    public reader(contents: string): Partial<RetObj> {
         //const firstPass = contents.split('\n');
         let firstPass = contents.replaceAll('\n', ',').split(',');
         firstPass = firstPass.filter((el) => {
@@ -135,10 +185,12 @@ export class MTFClass {
             return this.singeLines(item);
         });
 
-        const retObj = this.shallowPop(secondPass);
-
+        const firstResult = this.shallowPop(secondPass);
         const mutliChunks = this.multiLines(secondPass);
-        
+        const deepResult = this.deepPop(mutliChunks);
+
+        const retObj = {...deepResult, ...firstResult};
+
         return retObj;
     }
 
